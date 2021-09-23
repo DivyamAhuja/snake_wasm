c63c2e7d279813c3cbb92d90e6ded70c0cdfa040
Divyam Ahuja <ahujadivyam@gmail.com>
2021-09-23 22:11:14 +0530

    fix initialization of new game

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 
 class Game : public olc::PixelGameEngine
 {
+private:
+    olc::vi2d pos;
+    olc::vf2d direction;
+    float speed;
+    std::list<olc::vi2d> snake;
+    olc::vi2d food;
+    float time_passed;
+    
+    unsigned int score;  
+    int drop_food;
+    bool GameOver;
+    bool MainMenu;
+
+    const int GRID_WIDTH = 40, GRID_HEIGHT = 40;
+    std::string scoreString = "Score :";
+    const olc::vi2d pixels = {10, 10};
+
 public:
     Game()
     {
@@ -11,32 +28,33 @@ public:
         winTitle = sAppName;
     }
 
-    olc::vi2d pos;
-    olc::vf2d direction = {1.0f, 0.0f};
-    float speed = 20;
-    std::list<olc::vi2d> snake;
-    olc::vi2d food = {-1000, -1000};
-    float time_passed = 0;
-    const olc::vi2d pixels = {10, 10};
-    const int GRID_WIDTH = 40, GRID_HEIGHT = 40;
-    unsigned int score = 0;
-    std::string scoreString = "Score :";
-    int drop_food = 0;
-    bool GameOver = true;
-    bool MainMenu = true;
-    bool OnUserCreate()
+
+    
+    void init()
     {
+        snake.clear();
         pos = {int(GRID_WIDTH / 2), int(GRID_HEIGHT / 2)};
         snake.push_back(pos);
         GameOver = true;
         MainMenu = true;
+
+        direction = {1.0f, 0.0f};
+        food = {rand() % GRID_WIDTH, rand() % GRID_HEIGHT};
+        time_passed = 0;
+        score = 0;
+        drop_food = 0;
+    }
+
+    bool OnUserCreate()
+    {
+        init();
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime)
     {
         this->Clear(olc::BLACK);
-        
+
         if (!GameOver)
         {
             time_passed += fElapsedTime;
@@ -111,17 +129,20 @@ public:
                 DrawRect(x * pixels.x, pixels, olc::GREEN);
             }
         }
-        else if (MainMenu) {
-            DrawString(ScreenWidth()/ 2 - 44, ScreenHeight()/2, "Play Game", olc::RED);
-            DrawRect(ScreenWidth()/ 2 - 52, ScreenHeight()/2 - 5, 85, 20,olc::RED);
-            olc::vi2d p = { ScreenWidth()/ 2 - 120, 50};
+        else if (MainMenu)
+        {
+            DrawString(ScreenWidth() / 2 - 44, ScreenHeight() / 2, "Play Game", olc::RED);
+            DrawRect(ScreenWidth() / 2 - 52, ScreenHeight() / 2 - 5, 85, 20, olc::RED);
+            olc::vi2d p = {ScreenWidth() / 2 - 120, 50};
             DrawString(p, "SNAKE", olc::GREEN, 6);
-            if (this->GetMouse(olc::Mouse::LEFT).bPressed) {
-                olc::vi2d p1 = { ScreenWidth()/ 2 - 52, ScreenHeight()/2 - 5 };
-                olc::vi2d p2 = { ScreenWidth()/ 2 - 52 + 85, ScreenHeight()/2 - 5 + 20};
+            if (this->GetMouse(olc::Mouse::LEFT).bPressed)
+            {
+                olc::vi2d p1 = {ScreenWidth() / 2 - 52, ScreenHeight() / 2 - 5};
+                olc::vi2d p2 = {ScreenWidth() / 2 - 52 + 85, ScreenHeight() / 2 - 5 + 20};
                 int mouseX = this->GetMouseX();
                 int mouseY = this->GetMouseY();
-                if((p1.x <= mouseX && mouseX <= p2.x) && (p1.y <= mouseY && mouseY <= p2.y)) {
+                if ((p1.x <= mouseX && mouseX <= p2.x) && (p1.y <= mouseY && mouseY <= p2.y))
+                {
                     MainMenu = false;
                     GameOver = false;
                 }
@@ -132,8 +153,9 @@ public:
             // Game Over
             DrawString(ScreenWidth() / 2 - 160, ScreenHeight() / 2, "Game Over", olc::WHITE, 4);
             DrawString(10, 10, "Score :" + std::to_string(score));
-            if (this->GetMouse(olc::Mouse::LEFT).bPressed) {
-                MainMenu = true;
+            if (this->GetMouse(olc::Mouse::LEFT).bPressed)
+            {
+                init();
             }
         }
         return true;
